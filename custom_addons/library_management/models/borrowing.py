b44bd3753abd6bca0386883e6f709968c405f51e
qais <qaismih801@gmail.com>
2025-07-20 19:33:19 +0300

    Update all view files and fix XML tags to list

--- custom_addons/library_management/models/borrowing.py
+++ custom_addons/library_management/models/borrowing.py
@@ -1,4 +1,5 @@
 from odoo import models, fields, api
+from odoo.exceptions import ValidationError
 from datetime import timedelta
 
 class LibraryBorrowing(models.Model):
@@ -6,7 +7,7 @@ class LibraryBorrowing(models.Model):
     _description = 'Book Borrowing Record'
 
     book_id = fields.Many2one('library.book', string='Book', required=True)
-    borrower = fields.Many2one('res.partner', string='Borrower', required=True)
+    partner_id = fields.Many2one('res.partner', string='Borrower', required=True)
     borrow_date = fields.Date(string='Borrow Date', required=True, default=fields.Date.context_today)
     return_date = fields.Date(string='Return Date')
     returned = fields.Boolean(string='Returned', default=False)
@@ -16,5 +17,15 @@ class LibraryBorrowing(models.Model):
         if self.borrow_date:
             self.return_date = self.borrow_date + timedelta(days=7)
 
-    def mark_returned(self):
+    @api.model
+    def create(self, vals):
+        book = self.env['library.book'].browse(vals.get('book_id'))
+        if not book.available:
+            raise ValidationError("This book is currently not available for borrowing.")
+        book.available = False
+        return super().create(vals)
+
+    def action_mark_returned(self):
         self.returned = True
+        self.book_id.available = True
+
