7fb9d5cf463cd6be28017cd849b4f6ce761855a9
qais <qaismih801@gmail.com>
2025-07-30 09:53:12 +0300

    Initial commit - upload library_management module files

--- custom_addons/library_management/__manifest__.py
+++ custom_addons/library_management/__manifest__.py
@@ -2,31 +2,24 @@
     'name': 'Library Management',
     'version': '1.0',
     'summary': 'Manage books, authors, borrowing records, and memberships.',
-    'depends': ['base', 'account'],  # تم إضافة 'account' للفواتير
+    'depends': ['base', 'account', 'contacts'],  # إضافة contacts
     'data': [
-        # 🔐 ملفات الأمان
         'security/security.xml',
         'security/ir.model.access.csv',
-
-        
         'data/product_data.xml',
-
-        
+        'views/menu_views.xml',
         'views/res_partner_view.xml',
-
-        
         'views/book_views.xml',
         'views/author_views.xml',
         'views/borrowing_views.xml',
-
-    
         'views/membership_request_views.xml',
-        'views/library_menu.xml',
+        'views/library_menu.xml',            
         'views/library_actions.xml',
         'views/library_invoice_menu.xml',
     ],
     'installable': True,
     'application': True,
 }
+
 
 
